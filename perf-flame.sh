85d869e0435ab58b31c35e754c7155437864b07d
prajjawal05 <prajjawalagarwal@gmail.com>
2023-05-02 01:23:09 -0400

    added perf record for sys

--- perf-flame.sh
+++ perf-flame.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mallocs=("dh" "ff" "fg" "gd" "hd" "hm" "iso" "je" "lp" "lt" "mesh" "mi" "mng" "pa" "rp" "scudo" "sg"
-"sm" "sn" "tbb" "tc" "tcg")
-bms=("cfrac" "espresso" "barnes" "redis" "larson-sized" "mstress" "rptest" "gs" "lua")
-
+# mallocs=("dh" "ff" "fg" "gd" "hd" "hm" "iso" "je" "lf" "lp" "lt" "mesh" "mi" "mng" "nomesh" "pa" "rp" "sc" "scudo" "sg" "sm" "sn" "tbb" "tc" "tcg")
+# bms=("cfrac" "espresso" "barnes" "redis" "larson-sized" "mstress" "rptest" "gs" "lua")
+mallocs=("hd" "je" "sm" "tcg" "sys")
+bms=("cfrac" "espresso" "gs" "larson-sized" "lua" "redis")
 #lean
 
 for malloc in "${mallocs[@]}"
