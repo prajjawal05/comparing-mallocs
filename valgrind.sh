ee01a74c26213bc490b1f298c12b481d9377cf2c
prajjawal05 <prajjawalagarwal@gmail.com>
2023-05-02 14:58:12 -0400

    added valgrind logs

--- valgrind.sh
+++ valgrind.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mallocs=("dh" "ff" "fg" "gd" "hd" "hm" "iso" "je" "lp" "lt" "mesh" "mi" "mng" "pa" "rp" "scudo" "sg"
-"sm" "sn" "tbb" "tc" "tcg")
-bms=("cfrac" "espresso" "barnes" "redis" "larson-sized" "mstress" "rptest" "gs" "lua")
-
+# mallocs=("dh" "ff" "fg" "gd" "hd" "hm" "iso" "je" "lf" "lp" "lt" "mesh" "mi" "mng" "nomesh" "pa" "rp" "sc" "scudo" "sg" "sm" "sn" "tbb" "tc" "tcg")
+# bms=("cfrac" "espresso" "barnes" "redis" "larson-sized" "mstress" "rptest" "gs" "lua")
+mallocs=("hd" "je" "sm" "tcg" "sys")
+bms=("cfrac" "espresso" "gs" "larson-sized" "lua" "redis")
 #lean
 
 for malloc in "${mallocs[@]}"
@@ -11,8 +11,10 @@ do
     for bm in "${bms[@]}"
     do
          echo "Running valgrind for malloc: $malloc and bm: $bm"
-         log_file="$bm-$malloc-valgrind.txt"
+         log_file="results/$bm-$malloc-valgrind.txt"
          touch "$log_file"
-         sudo valgrind --log-file="$log_file" ../../bench.sh --procs=16 "$malloc" "$bm"
+         sudo ../../bench.sh --procs=16 "$malloc" "$bm" # requires change in run_test, add valgrind --log-file=valgrind.txt
+         mv valgrind.txt "$log_file"
+         # run using nohup ./myscript.sh > output.log 2>&1 &
     done
 done
