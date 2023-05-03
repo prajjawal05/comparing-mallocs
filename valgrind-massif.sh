314c9c6d5800354091436b6850f6a1719537208b
prajjawal05 <prajjawalagarwal@gmail.com>
2023-05-02 23:16:45 -0400

    added some massif results

---
+++ valgrind-massif.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# mallocs=("dh" "ff" "fg" "gd" "hd" "hm" "iso" "je" "lf" "lp" "lt" "mesh" "mi" "mng" "nomesh" "pa" "rp" "sc" "scudo" "sg" "sm" "sn" "tbb" "tc" "tcg")
+# bms=("cfrac" "espresso" "barnes" "redis" "larson-sized" "mstress" "rptest" "gs" "lua")
+mallocs=("hd" "je" "sm" "tcg" "sys")
+bms=("cfrac" "espresso" "gs" "larson-sized" "lua" "redis")
+#lean
+
+for malloc in "${mallocs[@]}"
+do
+    for bm in "${bms[@]}"
+    do
+         echo "Running valgrind massif for malloc: $malloc and bm: $bm"
+         log_file="results/$bm-$malloc-valgrind-massif.txt"
+         touch "$log_file"
+         sudo ../../bench.sh --procs=16 "$malloc" "$bm" # requires change in run_test, add valgrind --heap=yes --stacks=no --detailed-freq=100 --tool=massif
+         mv massif.out.* "$log_file"
+         # run using nohup ./myscript.sh > output.log 2>&1 &
+    done
+done
